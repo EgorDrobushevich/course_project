6366358acaf7a64011c8947c93115913556d0df0
EgorDrobushevich <120379629+EgorDrobushevich@users.noreply.github.com>
2023-05-10 09:45:12 +0300

    Add files via upload

---
+++ Cursach/Сайт/js/prices.js
@@ -0,0 +1,8 @@
+function befor(value){
+    const befor = document.getElementsByClassName("befor")[value];
+    befor.style="display:block";
+} 
+function nobefor(value){
+    const befor = document.getElementsByClassName("befor")[value];
+    befor.style="display:none";
+}
